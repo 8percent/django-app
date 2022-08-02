4e3a687b3bedf86bf90962ed92d4da77f51428dd
JaeyoungHeo <jay.jaeyoung@gmail.com>
2022-08-02 11:18:13 +0900

    Add django related settings

---
+++ tests/settings.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+TEST_DIR = Path(__file__).resolve().parent
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'holiday.apps.HolidayConfig',
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+SECRET_KEY = 'test-key'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': TEST_DIR / 'db.sqlite3',
+    }
+}
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_L10N = True
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
